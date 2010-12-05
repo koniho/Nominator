91eaf08b210dd5d254cc3cea1aecedc5e16d8d86
Alexander Kon-I Ho <koniho@gmail.com>
2010-12-04 19:41:44 -0500

    Initial comit of crap I made followin the RoR Guide

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'e918ef16f2828b7136a0ddc17c585e65dab9360e25fb6cf7d84403d9c456de765efba2fa4d9613b9186056199a87351a590e3b871ed390606a5b9432059f0352'
